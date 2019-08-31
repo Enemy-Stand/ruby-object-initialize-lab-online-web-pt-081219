a58a3869ecc70c078f666708dbbbff946013e02b
Joseph Sesti <sestijoey63@gmail.com>
2019-08-31 22:37:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(breed)
+  def initialize(breed, name)
     @breed = breed
+    @name = name
   end
   
   def breed=(breed)
@@ -9,5 +10,13 @@ class Dog
   
   def breed
     @breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
   end
 end
